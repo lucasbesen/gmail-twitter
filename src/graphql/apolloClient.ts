7bbd3ec1ccbcd7ceab5ceb64a5fad4051a604633
lucasbesen <lucassilvabesen@gmail.com>
2019-08-21 11:10:51 -0300

    chore(graphql): apolloClient config

---
+++ src/graphql/apolloClient.ts
@@ -0,0 +1,14 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const link = createHttpLink({
+  uri: "https://lucasbesen-twitter.herokuapp.com/graphql"
+});
+
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
+export default apolloClient;
