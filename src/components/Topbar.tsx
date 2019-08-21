d454ae02bcb8bf1912c3a2da03b5911b020ce194
lucasbesen <lucassilvabesen@gmail.com>
2019-08-21 17:04:58 -0300

    style(topbar): add apps, menu and help icons

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { debounce } from 'lodash';
+import { Menu } from 'styled-icons/material/Menu';
+import { Apps } from 'styled-icons/material/Apps';
+import { HelpCircle } from 'styled-icons/boxicons-regular/HelpCircle';
 
 import Logo from './Logo';
 import ProfilePic from './ProfilePic';
 import TextField from './TextField';
+import getFilterValue from '../utils/getFilterValue';
+
+const sharedStyle = css`
+  height: 25px;
+  color: #5f6368;
+  margin-right: 25px;
+  cursor: pointer;
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -16,12 +29,48 @@ const Wrapper = styled.div`
   border-bottom: 1px solid #eee;
 `;
 
-const Topbar: React.FC = () => (
-  <Wrapper>
-    <Logo />
-    <TextField />
-    <ProfilePic />
-  </Wrapper>
-);
+const InnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-export default Topbar;
+const MenuButton = styled(Menu)`
+  ${sharedStyle}
+`;
+
+const AppsButton = styled(Apps)`
+  ${sharedStyle}
+`;
+
+const HelpButton = styled(HelpCircle)`
+  ${sharedStyle}
+`;
+
+const Topbar: React.FC<RouteComponentProps> = ({ location, history }) => {
+  const [filter, setFilter] = useState(getFilterValue(location.search));
+
+  const onChangeFilter = debounce((value: string): void => {
+    history.push(`?search=${value}`);
+  }, 800);
+
+  const handleFilter = (value: string): void => {
+    onChangeFilter(value);
+    setFilter(value);
+  };
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <MenuButton />
+        <Logo />
+      </InnerWrapper>
+      <TextField defaultValue={filter} onChange={e => handleFilter(e.target.value)} />
+      <InnerWrapper>
+        <HelpButton />
+        <AppsButton />
+        <ProfilePic />
+      </InnerWrapper>
+    </Wrapper>
+  );
+};
+
+export default withRouter(Topbar);
