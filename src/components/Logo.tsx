6fd85dd56a802556eca462ed16df5033c8f7c9ac
lucasbesen <lucassilvabesen@gmail.com>
2019-08-20 23:23:06 -0300

    feat(topbar): create logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Logo = styled.img.attrs({
+  src: 'https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x.png',
+})`
+  width: 109px;
+  height: 40px;
+`;
+
+export default Logo;
