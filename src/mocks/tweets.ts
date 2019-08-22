4b54b9d15563f7ebd7125aa7bb487c2fd8edcd34
lucasbesen <lucassilvabesen@gmail.com>
2019-08-21 21:01:13 -0300

    test(tweet): tweet mock

---
+++ src/mocks/tweets.ts
@@ -0,0 +1,10 @@
+import { ITweet } from '../types';
+
+export const tweetMock: ITweet = {
+  id: '11111111',
+  text: 'This is a tweet',
+  user: {
+    name: 'User number 1',
+  },
+  created_at: 'Wed Aug 21 23:17:05 +0000 2019',
+};
